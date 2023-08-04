be390b60fa5a189b9fa29e55a12aa0cba5e2a270
safiyaritgithub <satyasafi79812@gmail.com>
2023-08-04 11:17:58 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ let [todoInput, updateInput] = useState()
+
+
+ let [todoList,updateTodos] = useState(
+  [
+    {
+      id:1,
+      task:'Learn React'
+
+    },
+    {
+      id:2,
+      task:'Learn Angular'
+
+    },
+    
+  ]
+)
+
+let nextId=3
+
+ function addNewTodo(){
+if(todoInput===""){
+  alert("add some text")
+}
+else{
+ let NewTodos=[
+    ...todoList,
+    {
+   id : nextId++,
+   task:todoInput
+
+    }
+  ]
+
+  updateTodos(NewTodos)
+  updateInput("")
+}
+}
+
+function deleteTodo(id){
+  let updatedTodos= todoList.filter(
+    (todo)=>{
+return todo.id!=id
+    }
+  )
+  updateTodos(updatedTodos)
+
+}
+
+
+
+ return (
+    <div className="container mt-5 w-50">
+      <h2 className="text-center font-semibold text-2xl mb-2">Todo App using React</h2>
+      <div className="input-group ">
+        <input type="text" className="form-control"onChange={(e)=>{
+          let task=e.target.value;
+          updateInput(task)
+
+        }} value={todoInput}/>
+        <button className="btn btn-primary" onClick={()=>{
+          addNewTodo()
+        }}>Add</button>
+        
+      </div>
+      <ul className="list-group mt-4">
+        {
+          todoList.map(
+            (todo)=>{
+              return( 
+                <li key={todo.id} className="list-group-item flex justify-between items-center">
+          <p>{todo.task}</p>
+          <button className="btn" onClick={()=>{
+            deleteTodo(todo.id)
+          }}>❌</button>
+        </li>
+              )
+        }
+          )
+        }
+        
+      </ul>
     </div>
   );
 }
